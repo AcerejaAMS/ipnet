b1a2907b790edab6a26e82d7fc43e59c075873a5
AcerejaAMS <amoraess1412@gmail.com>
2024-06-17 19:24:49 -0600

    Reinicio

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-
+import logo from './logo.svg';
 import './App.css';
-import express from "express";
-import dotenv from 'dotenv';
-import DB from "./backend/config/ds.js";
-import UsuarioRouter from "./backend/controllers/UsuarioController.js";
-import PublicacionRouter from "./backend/controllers/PublicacionController.js";
-import ConversacionRouter from "./backend/controllers/ConversacionController.js";
-import ComunidadRouter from "./backend/controllers/ComunidadController.js";
 
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 3000;
-console.log(port);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/", UsuarioRouter);
-app.use("/", PublicacionRouter);
-app.use("/", ConversacionRouter);
-app.use("/", ComunidadRouter);
-
-DB.connectDB(process.env.DB_URI);
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/api`);
-});
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
