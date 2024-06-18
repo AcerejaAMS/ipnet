28225889e4767277d8728197461e67a0e07bcfb5
AcerejaAMS <amoraess1412@gmail.com>
2024-06-17 21:11:55 -0600

    Cambios

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+        <img src="public\LogoIPNET.png" alt='logo'/>
+        <h1>IP_Net</h1>
+    </div>
+
+  );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+        <img src="public\LogoIPNET.png" alt='logo'/>
+        <h1>IP_Net</h1>
+    </div>
+
+  );
+};
+
+export default Home;
