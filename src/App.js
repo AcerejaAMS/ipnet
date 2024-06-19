c83718ac66b0d5d843cc1d8eb709ac5dc86231f4
AcerejaAMS <amoraess1412@gmail.com>
2024-06-18 23:05:28 -0600

    Paginas Principal Pre hecha

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Ingresar from './components/Ingresar';
 import Crear from './components/CrearCuenta';
+import PagPrincipal from './components/Principal';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Ingresar />} />
         <Route path="/crear" element={<Crear/>} />
+        <Route path="/pprincipal" element={<PagPrincipal/>} />
       </Routes>
     </Router>
   );
---
+++ src/components/Principal.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './aparienciaPrincipal.css';
+
+const PagPrincipal = () => {
+    const base = (
+        <body>
+            <div className="contencionCuerpo">
+                <header>
+                    <div className="logo">
+                        <img src="logoPW.png"/>
+                        <div className="busquedaContenedor">
+                            <input type="text" className="busquedaEntrada" placeholder="Buscar en IP_Net . . ."/>
+                        </div>
+                    </div>
+                    <nav>
+                        <a href="">
+                        <i className="fa-solid fa-bell"></i>
+                        </a>
+                        <a href="">
+                        <i className="fa-solid fa-message"></i>
+                        </a>
+                        <a href="">
+                        <i className="fa-solid fa-gear"></i>
+                        </a>
+                    </nav>
+                </header>
+                <div className="content">
+                    <div className="panelIzquierdo">
+                        <a className="contenedorOpcionesRS" href="">
+                            <i className="fa-solid fa-user"></i>
+                            <div>Perfil</div>
+                        </a>
+                        <a className="contenedorOpcionesRS" href="">
+                            <i className="fa-solid fa-house"></i>
+                            <div>Principal</div>
+                        </a>
+                        <a className="contenedorOpcionesRS" href="">
+                            <i className="fa-solid fa-users"></i>
+                            <div>Comunidades</div>
+                        </a>
+                        <a className="contenedorOpcionesRS" href="">
+                            <i className="fa-solid fa-address-book"></i>
+                            <div>Amigos</div>
+                        </a>
+                        <a className="contenedorOpcionesRS" href="">
+                            <i className="fa-solid fa-newspaper"></i>
+                            <div>Noticias</div>
+                        </a>
+                    </div>
+                    <div className="panelDerecho">
+                        <p>Referencia 1</p>
+                        <p>Referencia 2</p>
+                        <p>Referencia 3</p>
+                        <p>Referencia 4</p>
+                        <p>Referencia 5</p>
+                    </div>
+                </div>
+            </div>
+        </body>);
+    return(base);
+};
+
+export default PagPrincipal;
